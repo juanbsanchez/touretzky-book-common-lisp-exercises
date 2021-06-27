bb6c4ec4261e1bdc3665b49c66bd65e57d7e10db
Juan <53877929+juanbsanchez@users.noreply.github.com>
2021-06-27 13:29:02 +0200

    uncomment function calls

--- exercise1.lisp
+++ exercise1.lisp
@@ -4,8 +4,8 @@
     (x)
   (+ x 1))
 
-;; (add1 2) -> 3
-;; (add1 3) -> 4
+(add1 2) ;; -> 3
+(add1 3) ;; -> 4
 
 
 ;; Define add2 using add1.
@@ -14,8 +14,8 @@
     (x)
   (add1 (add1 x)))
 
-;; (add2 2) -> 4
-;; (add2 3) -> 5
+(add2 2) ;; -> 4
+(add2 3) ;; -> 5
 
 
 ;; Define a SUB2 function that subtracts two from its input.
@@ -24,8 +24,8 @@
     (x)
   (- x 2))
 
-;; (sub2 3) -> 1
-;; (sub2 4) -> 2
+(sub2 3) ;; -> 1
+(sub2 4) ;; -> 2
 
 
 ;; Write a predicate TWOMOREP that returns T if its first input is exactly two more than its second input. Use the ADD2 function in your definition of TWOMOREP.
@@ -34,8 +34,8 @@
     (x y)
   (equal (add2 x) y))
 
-;; (twomorep 3 5) -> T
-;; (twomorep 5 5) -> NIL
+(twomorep 3 5) ;; -> T
+(twomorep 5 5) ;; -> NIL
 
 
 ;; Find a way to write the TWOMOREP predicate using SUB2 instead of ADD2.
@@ -44,8 +44,8 @@
     (x y)
   (equal (sub2 y) x))
 
-;; (twomorep 5 7) -> T
-;; (twomorep 7 7) -> NIL
+(twomorep 5 7) ;; -> T
+(twomorep 7 7) ;; -> NIL
 
 
 ;; The average of two numbers is half their sum. Write the AVERAGE function.
@@ -54,8 +54,8 @@
     (x y)
   (/ (+ x y) 2))
 
-;; (average 4 10) -> 7
-;; (average 6 10) -> 8
+(average 4 10) ;; -> 7
+(average 6 10) ;; -> 8
 
 
 ;; Write a MORE-THAN-HALF-P predicate that returns T if its first input is more than half of its second input.
@@ -64,6 +64,6 @@
     (x y)
   (> x (/ y 2)))
 
-;; (more-than-half-p 2 4) -> NIL
-;; (more-than-half-p 8 14) -> T
+(more-than-half-p 2 4) ;; -> NIL
+(more-than-half-p 8 14) ;; -> T
 
