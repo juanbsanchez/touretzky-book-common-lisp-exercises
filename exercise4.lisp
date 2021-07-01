607d9ff1c306f622d8b22040f290d076686079bb
unknown <juanbsanchez1988@gmail.com>
2021-07-01 20:58:46 +0200

    continue exercise 4

--- exercise4.lisp
+++ exercise4.lisp
@@ -27,4 +27,29 @@
 (further -3) ;; => -4
 
 
+;; 4.3 Recall the primitive function NOT:
+;; It returns NIL for a true input and T for a false one.
+;; Suppose Lisp didn't have a NOT primitive. Show how to write NOT using jus IF and constants. (no other functions)
+;; Call your function MY-NOT
+
+(defun my-not (input) (if (equal input nil) t nil))
+
+(my-not nil) ;; => T
+(my-not t) ;; => NIL
+
+
+;; 4.4 Write a function ORDERED that takes two numbers as input and makes a list of them in ascending order.
+;; (ORDERED 3 4) should return the list (3 4).
+;; (ORDERED 4 3) should also return (3 4)
+;; in other words, the first and second inputs should appear in reverse order when the first is greater than the sencond
+
+(defun ordered (x y)
+  (if (> x y)
+      (list y x)
+      (list x y)))
+
+(ordered 3 4) ;; => (3 4)
+(ordered 4 3) ;; => (3 4)
+
+
 
