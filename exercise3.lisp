90bf2961ead6e40cef480142e1eada7a301e9ea2
unknown <juanbsanchez1988@gmail.com>
2021-07-01 11:05:23 +0200

    continue exercise 3

--- exercise3.lisp
+++ exercise3.lisp
@@ -17,4 +17,50 @@
 (+ ( * 3 3) ( * 4 4)) ;; => 25
 
 
+;; Write definitions for HALF, CUBE, and ONEMOREP using DEFUN.
+;; (THE CUBE function should take a number n as input and return n^3).
+
+(defun half (n)
+  (/ n 2))
+
+(half 3.0) ;; => 1.5
+(half 4) ;; => 2
+
+
+(defun cube (n)
+  (* n n n))
+
+(cube 3) ;; => 27
+(cube 4) ;; => 64
+
+
+(defun onemorep (x y)
+  (equal x (+ y 1)))
+
+(onemorep 3 4) ;; => NIL
+(onemorep 4 3) ;; => T
+
+
+;; Define a function PYTHAG that takes two inputs, x and y, and returns the square root of x^2+y^2. You may recognize this as Pythagora's formula for computing the length of the hypotenuse of a right triangle given the lengths of the other two sides. (PYTHAG 3 4) should return 5.0
+
+(defun pythag (x y)
+  (sqrt (+ (* x x) (* y y))))
+
+(pythag 3 4) ;; => 5.0
+
+
+;; Define a function MILES-PER-GALLON that takes three inputs, called
+;; INITIAL-ODOMETER-READING, FINAL-ODOMETER-READING,
+;; and GALLONS-CONSUMED, and computes the number of miles traveled per gallon of gas
+
+(defun miles-per-gallon
+    (initial-odometer-reading
+     final-odometer-reading
+     gallons-consumed)
+  ( / ( - final-odometer-reading
+          initial-odometer-reading)
+      gallons-consumed))
+
+(miles-per-gallon 10 100 10) ;; => 9
+(miles-per-gallon 55 60 3.0) ;; => 1.6666666666666667
 
