671b26a6bac721ac4d0586a124f2cffc847f6d10
unknown <juanbsanchez1988@gmail.com>
2021-07-01 19:20:09 +0200

    continue exercise 4

--- exercise4.lisp
+++ exercise4.lisp
@@ -1,3 +1,6 @@
+;; CONDITIONALS
+
+
 ;; 4.1 Write a function MAKE-EVEN that makes an odd number even by adding one to it.
 ;; If the input to MAKE-EVENT is already even, it should be returned unchanged.
 
