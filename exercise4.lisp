3a048d7cec6f629a2ecc63c3f7452fc21f589bab
unknown <juanbsanchez1988@gmail.com>
2021-07-01 21:29:27 +0200

    continue exercise 4

--- exercise4.lisp
+++ exercise4.lisp
@@ -62,4 +62,18 @@
 (my-abs -3) ;; => 3
 
 
+;; 4.8 Write EMPHASIZE3 which is like EMPHASIZE2 but adds the symbol VERY onto the list it it doesn't know how to emphasize it.
+;; For example, EMPHASIZE3 of (LONG DAY) should produce (VERY LONG DAY).
+;; What does EMPHASIZE3 of (VERY LONG DAY) produce?
+
+(defun emphasize3 (x)
+  (cond ((equal (first x) 'good) (cons 'great (rest x)))
+        ((equal (first x) 'bad) (cons 'awful (rest x)))
+        (t (cons 'very x))))
+
+(emphasize3 '(good day)) ;; => (great day)
+(emphasize3 '(bad day)) ;; => (awful day)
+(emphasize3 '(very long day)) ;; => (very very long day)
+
+
 
