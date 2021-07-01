8fbfd84694bf2996a78c0df6174dac972c06d759
unknown <juanbsanchez1988@gmail.com>
2021-07-01 19:18:59 +0200

    start exercise 4

---
+++ exercise4.lisp
@@ -0,0 +1,11 @@
+;; 4.1 Write a function MAKE-EVEN that makes an odd number even by adding one to it.
+;; If the input to MAKE-EVENT is already even, it should be returned unchanged.
+
+(defun make-even (n)
+  (if (oddp n)
+      (+ n 1)
+      n))
+
+(make-even 1) ;; => 2
+(make-even 2) ;; => 2
+
