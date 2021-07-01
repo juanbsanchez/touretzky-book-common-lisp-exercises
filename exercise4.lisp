a20660188995da067a86a38ed5f0784636879f8b
unknown <juanbsanchez1988@gmail.com>
2021-07-01 19:24:36 +0200

    continue exercise 4

--- exercise4.lisp
+++ exercise4.lisp
@@ -1,5 +1,5 @@
 ;; CONDITIONALS
-
+;; --------------------------------
 
 ;; 4.1 Write a function MAKE-EVEN that makes an odd number even by adding one to it.
 ;; If the input to MAKE-EVENT is already even, it should be returned unchanged.
@@ -11,4 +11,20 @@
 
 (make-even 1) ;; => 2
 (make-even 2) ;; => 2
+
+
+;; 4.2 Write a function FURTHER that makes a positive number larger by adding one to it,
+;; and a negative number smaller by subtracting one from it.
+;; What does your function do if given the number 0? => add one
+
+(defun further (n)
+  (if (< n 0)
+      (- n 1)
+      (+ n 1)))
+
+(further 2) ;; => 2
+(further 0) ;; => 1
+(further -3) ;; => -4
+
+
 
