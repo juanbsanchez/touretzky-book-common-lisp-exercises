4bb3352f3ed2be2a409650859c858a959eeff412
juanbsanchez <juanbsanchez1988@gmail.com>
2021-07-17 21:02:31 +0200

    continue exercise 9

--- exercise9.lisp
+++ exercise9.lisp
@@ -71,3 +71,48 @@
 (defun draw-line (n)
   (cond ((zerop n) (format t "~%"))
 	(t (format t "*") (draw-line (- n 1)))))
+
+
+;; 9.3. Write a recursive function DRAW-BOX that calls DRAW-LINE
+;; repeatedly to draw a box of specified dimensions. (DRAW-BOX 10 4)
+;; should produce
+;; **********
+;; **********
+;; **********
+;; **********
+
+(defun draw-box (width height)
+  (cond ((zerop height) nil)
+	(t (draw-line width)
+	   (draw-box width (- height 1)))))
+
+;; 9.4. Write a recursive function NINETY-NINE-BOTTLES that sings the
+;; well-known song ‘‘Ninety-nine Bottles of Beer on the Wall.’’ The first
+;; verse of this song is
+;; 99 bottles of beer on the wall,
+;; 99 bottles of beer!
+;; Take one down,
+;; Pass it around,
+;; 98 bottles of beer on the wall.
+
+;; NINETY-NINE-BOTTLES should take a number N as input and start
+;; counting from N down to zero. (This is so you can run it on three
+;; bottles instead of all ninety nine.) Your function should also leave a
+;; blank line between each verse, and say something appropriate when it
+;; runs out of beer.
+
+(defun ninety-nine-bottles (n)
+  (cond ((zerop n)
+	 (format t "~&Awww, no more beer!"))
+	(t (do-verse n)
+	   (ninety-nine-bottles (- n 1)))))
+
+(defun do-verse (n)
+  (format t
+	  "~&~S bottles of beer on the wall, ~%" n)
+  (format t "~S bottles of beer!~%" n)
+  (format t "Take one down, ~%Pass it around,~%")
+  (format t
+	  "~S bottles of beer on the wall. ~%~%"
+	  (- n 1)))
+
