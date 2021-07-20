bc0c35d056df033430b9c699b5c242832a8efd71
juanbsanchez <juanbsanchez1988@gmail.com>
2021-07-20 22:40:30 +0200

    continue exercise 9

--- exercise9.lisp
+++ exercise9.lisp
@@ -178,6 +178,8 @@
 	      "~&The worker earned ~S dollars."
 	      (* wage hours)))))
 
+;; 9.7. The COOKIE-MONSTER function keeps reading data from the
+;; terminal until it reads the symbol COOKIE. Write COOKIEMONSTER. Here is a sample interaction:
 
 (defun cookie-monster ()
   (format t "~&~%Give me a cookie!!! ~&Cookie? ")
@@ -187,3 +189,92 @@
 	  (t
 	   (format t "No want ~S...~%" x)
 	   (cookie-monster)))))
+
+;; The YES-OR-NO-P function takes a format control string as input and asks
+;; the user a yes or no question. The user must respond by typing ‘‘yes,’’ in
+;; which case the function returns T, or ‘‘no,’’ in which case it returns NIL.
+
+(defun riddle ()
+  (if (yes-or-no-p
+       "Do you seek Zen enlightenment? ")
+      (format t "Then do not ask for it!")
+      (format t "You have found it. ")))
+
+;; There is also a shorter form of this function, called Y-OR-N-P, that only
+;; requires the user to type ‘‘y’’ or ‘‘n’’ in response.
+
+;; The WITH-OPEN-FILE macro provides a convenient way to read data from a
+;; file. Its syntax is:
+;; (WITH-OPEN-FILE (var pathname)
+;; body)
+
+(defun get-tree-data ()
+ (with-open-file (stream "/usr/dst/timber.dat")
+   (let* ((tree-loc (read stream))
+	  (tree-table (read stream))
+	  (num-trees (read stream)))
+     (format t "~&There are ~S trees on ~S."
+	  num-trees tree-loc)
+     (format t "~&They are: ~S" tree-table))))
+
+;; We can also use WITH-OPEN-FILE to open files for output by passing it the
+;; special keyword argument :DIRECTION :OUTPUT.
+;; The stream that WITHOPEN-FILE creates can then be used in place of the usual T as a first
+;; argument to FORMAT.
+
+(defun save-tree-data (tree-loc tree-table
+		       num-trees)
+  (with-open-file (stream "/usr/dst/timber.newdat"
+			  :direction :output)
+    (format stream "~S~%" tree-loc)
+    (format stream "~S~%" tree-table)
+    (format stream "~S~%" num-trees)))
+
+;; (save-tree-data
+;; "The West Ridge"
+;; ’((45 redwood) (22 oak) (43 maple))
+;; 110)
+;; NIL
+
+
+
+;; In this exercise we will write a program for producing a graph of an arbitrary
+;; function. The program will prompt for a function name F and then plot y =
+;; F(x) for a specified range of x values. Here is an example of how the program
+;; works:
+
+;; > (make-graph)
+;; Function to graph? square
+;; Starting x value? -7
+;; Ending x value? 7
+;; Plotting string? "****"
+
+;; 9.10. As you write each of the following functions, test it by calling it from
+;; top level with appropriate inputs before proceeding on to the next
+;; function.
+
+;; a. Write a recursive function SPACE-OVER that takes a number N as
+;; input and moves the cursor to the right by printing N spaces, one at a
+;; time. SPACE should print ‘‘Error!’’ if N is negative. Test it by
+;; using the function TEST. Try (TEST 5) and (TEST −5).
+;; (defun test (n)
+;; (format t "~%>>>")
+;; (space-over n)
+;; (format t "<<<"))
+
+(defun space-over (n)
+  (cond ((plusp n)
+	 (format t " ")
+	 (space-over (- n 1)))
+	((zerop n) nil)
+	(t (format t "Error!"))))
+
+;; b. Write a function PLOT-ONE-POINT that takes two inputs,
+;; PLOTTING-STRING and Y-VAL, prints PLOTTING-STRING
+;; (without the quotes) in column Y-VAL, and then moves to a new
+;; line. The leftmost column is numbered zero.
+
+(defun plot-one-point (plotting-string y-val)
+  (space-over y-val)
+  (format t "~A~%" plotting-string))
+
