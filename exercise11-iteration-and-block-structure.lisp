f872706bb9835cb515ed9b3ad5f94f198dc5b062
juanbsanchez <juanbsanchez1988@gmail.com>
2021-07-26 20:13:49 +0200

    continue exercise 11

--- exercise11-iteration-and-block-structure.lisp
+++ exercise11-iteration-and-block-structure.lisp
@@ -102,8 +102,86 @@
 ;; 11.10. Show how to write LAUNCH using DOTIMES.
 
 (defun launch (n)
-  (dotimes (i n 1)
+  (dotimes (i n)
     (format t "~S..." (- n i)))
     (format t "Blast off!"))
 
-(launch 10)
+
+;; Infinite loop with DO, specifying NIL as the termination test.
+;; If user types something other than a number, the function prints an error message
+;; and again watis for input. If user does type a number, the function exists the loop using RETURN to return the number.
+
+(defun read-a-number ()
+  (do ((answer nil))
+      (nil)
+    (format t "~&Please type a number: ")
+    (setf answer (read))
+    (if (numberp answer)
+	(return answer))
+    (format t
+	    "~&Sorry, ~S is not a number. Try again."
+	    answer)))
+
+
+;; In this keyboard exercise we will explore some properties of single- and
+;; double-stranded DNA, or deoxyribonucleic acid. DNA, and the related
+;; molecule RNA, make up the genetic material found in viruses and every type of cell, from bacteria to people. A strand of DNA is very much like a chain of
+;; cons cells. The elements of the chain are of four types, corresponding to the
+;; four bases adenine, thymine, guanine, and cytosine. We will represent a
+;; strand of DNA by a list of bases. The list (A G G T C A T T G) corresponds
+;; to a strand that is nine bases long. The first base being adenine and the next two
+;; guanine. Here is a schematic diagram of the strand:
+
+
+;; ------------------
+;; ! ! ! ! ! ! ! ! !
+;; A G G T C A T T G
+
+;; Each of the four bases has a complement with which it can form a pair.
+;; Adenine pairs with thymine, while guanine pairs with cytosine. Two single
+;; strands of DNA can combine to form double-stranded DNA (whose shape is
+;; the famous ‘‘double helix’’) when each of their corresponding bases are
+;; complementary. The strand (A G G T C A T T G) and the strand (T C C A G
+;; T A A C) are complementary, for example. Double-stranded DNA looks like
+;; this:
+
+;; -----------------
+;; ! ! ! ! ! ! ! ! !
+;; A G G T C A T T G
+;; . . . . . . . . .
+;; . . . . . . . . .
+;; T C C A G T A A C
+;; ! ! ! ! ! ! ! ! !
+;; -----------------
+
+;; 11.22. Write iterative solutions to all parts of this exercise that require
+;; repetitive actions.
+;; a. Write a function COMPLEMENT-BASE that takes a base as input
+;; and returns the matching complementary base. (COMPLEMENTBASE ’A) should return T; (COMPLEMENT-BASE ’T) should
+;; return A; and so on.
+
+;; cond solution
+(defun complement-base (x)
+  (cond ((equal x 'a) 't)
+	((equal x 'g) 'c)
+	((equal x 't) 'a)
+	((equal x 'c) 'g)
+	(t nil)))
+
+;; assoc solution
+(defun complement-base (base)
+  (second
+   (assoc base '((a t) (t a) (g c) (c g)))))
+
+;; iterative solution
+(setf base
+      '((a t)
+	(t a)
+	(g c)
+	(c g)))
+
+(defun complement-base (x)
+  (dolist (e base)
+    (if (equal x (first e)) (return (second e)))))
+
+
